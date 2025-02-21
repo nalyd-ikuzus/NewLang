e6f37c6c70bb6c48c895a01edc18aaa19fa30b62
Donovan Felton <dfelton42@gmail.com>
2025-02-21 14:44:32 -0800

    parser

--- src/parser.js
+++ src/parser.js
@@ -1,3 +1,11 @@
-export default function parse(sourceCode) {
-    throw new Error("Not implemented")
+import * as fs from “fs”;
+import * as ohm from “ohm-js”;
+const grammar = ohm.grammer(fs.readFileSync(“src/newlang.ohm”, “utf8"));
+export default function parse(sourceCodeFileName) {
+  const sourceCode = fs.readFileSync(sourceCodeFileName, “utf8”);
+  const match = grammar.match(sourceCode);
+  if (match.failed()) {
+    throw match.message;
   }
+  return match;
+}
